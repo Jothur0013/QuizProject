de5e1c2271efdba385a6ee08637a5fabc9b81754
DaviCab3 <davicabeceira1@gmail.com>
2026-07-07 18:05:20 -0300

    AURA + EGO + 67

--- src/quiz/aplicacao/console/MeuquizConsole.java
+++ src/quiz/aplicacao/console/MeuquizConsole.java
@@ -1,5 +1,6 @@
 package quiz.aplicacao.console;
 
+import quiz.framework.PerguntaFactory;
 import quiz.framework.QuizTemplate;
 import quiz.framework.Pergunta;
 
@@ -8,13 +9,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MeuquizConsole extends QuizTemplate {
-    @Override
-    protected List<Pergunta> carregarPerguntas(){
-        List<Pergunta> perguntasJ = new ArrayList<>();
-        perguntasJ.add(new Pergunta("Orcas são?", Arrays.asList("Baleias", "Peixes", "Golfinhos", "Tubaroes" ), "Golfinhos"));
-        perguntasJ.add(new Pergunta("Qual o maior time brasileiro na libertadores?", Arrays.asList("Flamengo", "Fluminense", "Palmeiras", "Sao Paulo"), "Flamengo"));
-        perguntasJ.add(new Pergunta("Quem é um pouco mais louco que todos nós?", Arrays.asList("ShigeoNomura", "Monark", "Lula", "Bolsonaro"), "ShigeoNomura"));
-        return perguntasJ;
-    }
 
+public MeuquizConsole () {
+    super(new PerguntaFactoryConsole());
+
+    }
 }
+
+
--- src/quiz/framework/QuizTemplate.java
+++ src/quiz/framework/QuizTemplate.java
@@ -1,29 +1,53 @@
 package quiz.framework;
 
 import java.util.List;
+
 public abstract class QuizTemplate {
-    public int acertos = 0, erros = 0, pontFinal = 0;
+
+    // A lista de perguntas que o framework vai gerenciar
+    protected List<Pergunta> perguntas;
+
+
+    public QuizTemplate(PerguntaFactory fabricaDePerguntas) {
+        this.perguntas = fabricaDePerguntas.criarPerguntas();
+    }
+
+    // Atributos da classe para o controle de pontuação
+    public int acertos = 0;
+    public int erros = 0;
+    public int pontFinal = 0;
+
+    // O Template Method que dita o fluxo do jogo
     public final void iniciarQuiz(QuizUI interfaceGrafica, PontuacaoStrategy estrategiadePontos){
-        int acertos = 0, erros = 0, pontFinal = 0;
-        List<Pergunta> perguntasQuiz = carregarPerguntas();
 
-    for (Pergunta pergunta : perguntasQuiz){
-        interfaceGrafica.exibePergunta(pergunta);
-        String respostaUsuario = interfaceGrafica.usuarioResposta();
-        while(!interfaceGrafica.analisePergunta(pergunta))
-            {respostaUsuario = interfaceGrafica.usuarioResposta();}
-        boolean acertou = pergunta.getResposta().equalsIgnoreCase(respostaUsuario);
+        // Iteramos diretamente sobre a lista "this.perguntas" já carregada
+        for (Pergunta pergunta : this.perguntas) {
 
-        if (acertou){
-            acertos++;}
-        else {
-            erros++;}
-        interfaceGrafica.feedback(acertou);
+            interfaceGrafica.exibePergunta(pergunta);
+            String respostaUsuario = interfaceGrafica.usuarioResposta();
 
-        pontFinal += estrategiadePontos.calculaPontuacao(acertou);};
-    interfaceGrafica.exibePontuacaoFinal(acertos, erros, pontFinal);}
+            // Loop para garantir que o usuário digite uma opção válida
+            while(!interfaceGrafica.analisePergunta(pergunta)) {
+                respostaUsuario = interfaceGrafica.usuarioResposta();
+            }
 
-    protected abstract List<Pergunta> carregarPerguntas();
+            // Verifica se a resposta está correta
+            boolean acertou = pergunta.getResposta().equalsIgnoreCase(respostaUsuario);
 
+            // Atualiza as variáveis da classe (usando o "this.")
+            if (acertou) {
+                this.acertos++;
+            } else {
+                this.erros++;
+            }
 
+            interfaceGrafica.feedback(acertou);
+
+            // Usa o Strategy para calcular a pontuação
+            this.pontFinal += estrategiadePontos.calculaPontuacao(acertou);
+        }
+
+        // Exibe o resultado no final do quiz
+        interfaceGrafica.exibePontuacaoFinal(this.acertos, this.erros, this.pontFinal);
+    }
 }
